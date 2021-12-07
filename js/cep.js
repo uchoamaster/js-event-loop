9d4966031e4448d1a0fed8665a90b337377ffe7e
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-12-07 16:58:08 -0300

    fazendo requisição via api

--- js/cep.js
+++ js/cep.js
@@ -1,5 +1,24 @@
+const configs = {
+    method: 'GET'
+}
 const searchCep = (event) => {
-    alert('Buscando o cep')
+
+    const cep = document.getElementById('cep').value || '000000000'
+
+    // alert('Buscando o cep')
+    fetch(`https://ws.apicep.com/cep/${cep}.json`,configs)
+    .then(response =>{
+        // console.log(response)
+        return response.json()
+    })
+    .then(data => {
+        console.log(data)
+
+        if(data.status === 0) throw data.message
+    })
+    .catch(error => {
+        console.log(error)
+    })
 
     event = event || window.event
     if( event.preventDefault) event.preventDefault()
